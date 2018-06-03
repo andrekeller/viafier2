a17f29a9b858587ede2d8e07694d3c5194148d5d
André Keller <andre.keller@vshn.ch>
2018-06-03 17:17:23 +0200

    Simplify currency filter to avoid locale issues

--- viafier2/common/templatetags/currency.py
+++ viafier2/common/templatetags/currency.py
@@ -1,9 +1,6 @@
 from django import template
-from django.utils.translation import get_language
-from django.utils.translation import to_locale
-import locale
+from django.conf import settings
 
-locale.setlocale(locale.LC_ALL, locale="{}.utf8".format(to_locale(get_language())))
 register = template.Library()
 
 
@@ -11,4 +8,7 @@ register = template.Library()
 def currency(value):
     if value is None:
         return None
-    return locale.currency(value, grouping=True)
+    return "{} {:.2f}".format(
+        settings.CURRENCY,
+        value
+    )
