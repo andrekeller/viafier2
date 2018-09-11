73157bd850e57c8bb0acd604dcb3fd182fe0bc20
André Keller <ak@0x2a.io>
2018-09-11 10:35:54 +0200

    Fix layout and URL patterns

--- viafier2/viafier2/urls.py
+++ viafier2/viafier2/urls.py
@@ -11,15 +11,18 @@ from django.urls import path
 from django.utils.translation import gettext_lazy as _
 
 
-urlpatterns = i18n_patterns(
-    path('', include('common.urls')),
+urlpatterns = [
     path('admin/', admin.site.urls),
+    path('taggit/', include('taggit_selectize.urls')),
+    path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /",
+                                              content_type="text/plain")),
+]
+
+urlpatterns += i18n_patterns(
+    path('', include('common.urls')),
     path(_('inventory/'), include('inventory.urls')),
     path(_('login/'), LoginView.as_view(template_name='login.html'), name='login'),
     path(_('logout/'), LogoutView.as_view(), name='logout'),
-    path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /",
-                                              content_type="text/plain")),
-    path('taggit/', include('taggit_selectize.urls')),
     prefix_default_language=False,
 )
 
